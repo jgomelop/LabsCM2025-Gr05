e2b84f28bfb7a6576d1d24e571a4cdb3ea161134
JuanPabloGomezLopez <juan.gomez148@udea.edu.co>
2025-04-23 19:38:38 -0500

    Añadiendo Titulo a PersonalDataActivity.kt

--- Lab01-UI/src/main/java/co/edu/udea/compumovil/gr05_20251/lab1/PersonalDataActivity.kt
+++ Lab01-UI/src/main/java/co/edu/udea/compumovil/gr05_20251/lab1/PersonalDataActivity.kt
@@ -7,8 +7,16 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 import co.edu.udea.compumovil.gr05_20251.lab1.ui.theme.Labs20251Gr05Theme
 
@@ -16,19 +24,46 @@ const val PERSONAL_DATA_TAG = "PersonalActivity"
 class PersonalDataActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val intent = Intent(this, ContactDataActivity::class.java)
         Log.d(PERSONAL_DATA_TAG,"onCreate PersonalActivity")
         enableEdgeToEdge()
+
         setContent {
             Labs20251Gr05Theme {
-                Button(onClick = {
-                    val intent = Intent(this, ContactDataActivity::class.java)
-                    startActivity(intent)
-                }) {
-                    Text("Siguiente")
+                Column {
+                    PersonalDataForm()
+                    Button(onClick = {
+                        startActivity(intent)
+                    }) {
+                        Text("Siguiente")
+                    }
                 }
+
             }
         }
     }
+}
+
+@Composable
+fun PersonalDataForm() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(6.dp)
+    ) {
+        FormTitle()
+        HorizontalDivider(thickness = 2.dp)
+    }
+}
+
+@Composable
+fun FormTitle() {
+    Text(
+        text = "Información Personal",
+        modifier = Modifier.padding(vertical = 16.dp)
+    )
 }
 
 
